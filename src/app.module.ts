1795e9e472556aed7d94305285ba22033f13b8e3
afraca130 <zokatn@gmail.com>
2024-12-14 20:30:47 +0900

    feat: postgreSQL 연결 및 유저테이블 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtAuthGuard } from './modules/common/guards/jwt.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmConfigService } from './modules/common/config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', `.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [
     {
---
+++ src/modules/common/config/typeorm.config.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as fs from 'fs';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  private ENTITY_LOCATION = 'dist/src/modules/**/*.entity.{js, d.ts}';
+  private TEST_ENTIYT_LOCATION = 'src/modules/**/*.entity.{js,ts}';
+  private MIGRATION_LOCATION = 'dist/src/migrations/*{.ts,.js}';
+  private SEED_LOCATION = 'dist/src/database/seeds/**/*.js';
+  private FACTORY_LOCATION = 'dist/src/database/factories/**/*.js';
+
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log('DB Config:', {
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'), // 비밀번호 확인
+      database: this.configService.get<string>('DB'),
+    });
+
+    const postgresOption: PostgresConnectionOptions = {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB'),
+    };
+
+    return {
+      ...postgresOption,
+      entities: [this.getEntityLocation()],
+      synchronize: this.configService.get<boolean>('DB_SYNCHRONIZE'),
+      migrations: [this.MIGRATION_LOCATION],
+      migrationsTableName: 'migrations',
+    };
+  }
+
+  getEntityLocation() {
+    const isTest = this.configService.get<string>('NODE_ENV') === 'TEST';
+    if (isTest) {
+      return this.TEST_ENTIYT_LOCATION;
+    }
+    return this.ENTITY_LOCATION;
+  }
+
+  // setSeedConfig() {
+  //   return {
+  //     seeds: [this.SEED_LOCATION],
+  //     factories: [this.FACTORY_LOCATION],
+  //   };
+  // }
+
+  // setOrmConfigFile(): void {
+  //   const totalConfigs = {
+  //     ...this.createTypeOrmOptions(),
+  //     ...this.setSeedConfig(),
+  //   };
+
+  //   const hasFile = fs.existsSync('ormconfig.json');
+  //   if (hasFile) {
+  //     fs.unlinkSync('ormconfig.json');
+  //   }
+  //   fs.writeFileSync('ormconfig.json', JSON.stringify(totalConfigs, null, 2));
+  // }
+}
